8b190e766925d4de82cfd7aaa26a72b879a1190b
th-release <cth.release@gmail.com>
2024-08-21 11:01:42 +0900

    feat: Remove Ownable

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MyToken is ERC20, ERC20Burnable, ERC20Permit {
